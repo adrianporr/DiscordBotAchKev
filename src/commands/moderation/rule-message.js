73dba60fa65cfc1be610c1572eac4437892e67b1
adrianporr <adrianporr98@gmail.com>
2023-10-29 19:52:55 +0100

    added embed for rules cmd

--- src/commands/moderation/rule-message.js
+++ src/commands/moderation/rule-message.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'rule-message',
@@ -13,12 +13,31 @@ module.exports = {
             { // Community Role
                 id: "1167746037478326272",
                 label: "Regeln akzeptieren!"
-            },
-            { // Weeb Role 
-                id: "1168247103127048253",
-                label: "Weeb Rolle"
             }
         ]
+
+        const rulesEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle('REGELWERK')
+            .addFields(
+                { name: '[1] Illegale Aktivitäten', value: '> Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert', inline: true},
+                { name: '[2] Illegale Aktivitäten', value: '> Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert', inline: true},
+                { name: '\u200B', value: '\u200B' },
+                { name: '[3] Illegale Aktivitäten', value: '> Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert', inline: true},
+                { name: '[4] Illegale Aktivitäten', value: '> Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert', inline: true},
+                { name: '\u200B', value: '\u200B' },
+                { name: '[5] Illegale Aktivitäten', value: '> Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert', inline: true},
+                { name: '[6] Illegale Aktivitäten', value: '> Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert', inline: true},
+                { name: '\u200B', value: '\u200B' },
+                { name: '[7] Illegale Aktivitäten', value: '> Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert', inline: true},
+                { name: '[8] Illegale Aktivitäten', value: '> Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert', inline: true},
+                { name: '\u200B', value: '\u200B' },
+                { name: '\u2705 Server freischalten', value: '> Wenn du die Regeln gelesen und dem Server beitreten möchtest, bestätige mit dem unteren Button die Regeln' },
+            )
+
+        const rulesImage = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setImage('https://i.imgur.com/jwCrEeT.png')
 
         try {
             const channel = client.channels.cache.get('1167752003540242442');
@@ -36,10 +55,9 @@ module.exports = {
             })
     
             await channel.send({
-                content: `
-                **REGELWERK**\n\n**[1]** Illegale Aktivitäten - Jegliche Art von Gesetzesverstößen oder Verstöße gegen die Discord Guidelines werden nicht toleriert!\n**[2]** Respektvoller Umgang miteinander ist mir persönlich ein sehr wichtiges Anliegen!\n**[3]** Alle Beleidigungen, rassistischen, rechtsextremistischen oder sexistischen Aussagen sind in jeder Art und Weise untersagt und werden keineswegs toleriert.
-                `,
+                content: "",
                 components: [row],
+                embeds: [rulesImage, rulesEmbed]
             });
             process.exit
         } catch (error) {
